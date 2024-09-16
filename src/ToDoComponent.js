859d5226dcdd60a1f9203d1cc84ddbbdf2e79919
tejal23kadam <154776489+tejal23kadam@users.noreply.github.com>
2024-09-16 12:15:54 +0530

    project done

--- src/ToDoComponent.js
+++ src/ToDoComponent.js
@@ -1,14 +1,13 @@
 import { useReducer, useState } from 'react';
 
 function createInitialState() {
-
-
   return {
     todoId: '',
     draft: '',
     todos: [],
   };
 }
+
 
 let nextID = 0;
 function reducer(state, action) {
@@ -26,15 +25,18 @@ function reducer(state, action) {
         draft: '',
         todos: [{ todoId: nextID, draft: state.draft }, ...state.todos]
       };
-
     }
 
     case 'update_todo': {
+      //handleEditing();     
       return {
         ...state,
         todos: [...state.todos.map((todo) => {
-          if (todo.todoId === action.toUpdateId) {
-            // setEditing(true);
+
+          console.log("object : " + JSON.stringify(action));
+          console.log("id :" + JSON.stringify(todo.todoId));
+          if (todo.todoId === action.toUpdateId.todoId) {
+
             return { ...todo, draft: action.task };
           } else {
             return todo;
@@ -47,7 +49,6 @@ function reducer(state, action) {
         ...state,
         todos: [...state.todos.filter(item => item.todoId !== action.toDeleteId)]
       }
-
     }
     default: {
       throw new Error('Unknown action: ' + action.type);
@@ -57,38 +58,96 @@ function reducer(state, action) {
 
 export default function ToDoComponent() {
   const [state, dispatch] = useReducer(reducer, createInitialState());
-  const [editing, setEditing] = useState(false); //to set edit mode  or view mode (enable disable input for update)
-  let viewMode = {};
-  let editMode = {};
-  if (editing) {
-    viewMode.display = 'none'
+  const [selectedId, setSelectedID] = useState(-1);
+  const [idEdit, setIdEdit] = useState(null); //<== Create this state
+  const [dixing, setDixing] = useState(false);
+  let dix = {};
+  const seteditmode = () => {
+    if (dixing) {
+      dix.display = 'none';
+      setIdEdit(null);
+    }
+    else {
+      dix.display = 'block';
+      setIdEdit(null);
+    }
   }
-  else {
-    editMode.display = 'none';
-  }  
+
+  // return (
+  //   <>
+  //     <input value={state.draft} onChange={e => { dispatch({ type: 'input_draft', nextDraft: e.target.value }) }} />
+  //     <button onClick={() => { dispatch({ type: 'added_todo' }); }}>Add</button>
+  //     <ul>
+  //       {state.todos.map(item => (
+  //         <li key={item.todoId}>
+  //           <div style={{ display: 'flex' }}>
+  //             <div style={viewMode} >
+  //               {item.draft}
+  //               <button onClick={() => setEditing(true)} onChange={(e) => dispatch({ type: 'update_todo', task: e.target.value, toUpdateId: item })}>Update</button>
+  //             </div>
+  //             <div style={editMode}>
+  //               <input type='text' value={item.draft} onChange={e => { dispatch({ draft: e.target.value }) }} />
+  //               <button onClick={() => setEditing(false)}>save</button>
+  //             </div>
+  //             <button onClick={() => { dispatch({ type: 'delete_todo', toDeleteId: item.todoId }); }}>Delete</button>
+  //           </div>
+  //         </li>
+  //       ))}
+  //     </ul>
+  //   </>
+
+  // );
+
+
+
   return (
     <>
-      <input value={state.draft} onChange={e => { dispatch({ type: 'input_draft', nextDraft: e.target.value }); }} />
+      <input value={state.draft} onChange={e => { dispatch({ type: 'input_draft', nextDraft: e.target.value }) }} />
       <button onClick={() => { dispatch({ type: 'added_todo' }); }}>Add</button>
-       <ul>
-        {state.todos.map(item => (
-           <li key={item.todoId}>
-             <div style={{display: 'flex'}}>
-              <div style={viewMode} >                
-                {item.draft}
-                <button onClick={() => setEditing(true)}>Update</button>                
-              </div>
-              <div style={editMode}>
-                <input type='text' value={item.draft}
-                  onChange={(e) => { dispatch({ type: 'update_todo', task: e.target.value, toUpdateId: item.todoId }) }} />  
-                  <button onClick={() => setEditing(false)}>save</button>                    
-              </div>
+      <ul>
+        {state.todos.map((item, idx) => (
+          <li key={item.todoId} style={{ display: 'flex' }}>
+            <div style={{ display: 'flex' }}>
+              {
+                (selectedId === idx) ? (
+                  <div style={{ display: 'flex' }} >
+                    <input type='text'
+                      style={{ display: idEdit === item.todoId ? 'block' : 'none' }}
+                      value={item.draft}
+                      onChange={(e) => dispatch({ type: 'update_todo', task: e.target.value, toUpdateId: item })}
+                    />
+                    <h2 style={{ display: idEdit === item.todoId ? 'none' : 'block' }}>{item.draft}</h2>
+                    {
+                      (dixing) ? (
+                        <button style={{ 'margin-top': '30px' }} onClick={() => { seteditmode(); setDixing(false) }}  >save</button>
+                      )
+                     :
+                      (                      
+                        <button style={{ 'margin-top': '30px' }} onClick={() => { setSelectedID(idx); setIdEdit(item.todoId); setDixing(true) }}
+                            onChange={(e) => dispatch({ type: 'update_todo', task: e.target.value, toUpdateId: item })}>edit</button>
+                      )
+                    }
+
+                  </div>
+                ) : (
+                  <div style={{ display: 'flex', dixing }}>
+                    <div >
+                      <h2>{item.draft}</h2>
+                    </div>
+                    <div style={{ 'padding-top': '30px' }} >
+                      <button onClick={() => { setSelectedID(idx); setIdEdit(item.todoId); setDixing(true) }}
+                        onChange={(e) => dispatch({ type: 'update_todo', task: e.target.value, toUpdateId: item })}>edit</button>
+                    </div>
+                  </div>
+                )
+              }
+            </div>
+            <div style={{ 'padding-top': '30px' }}>
               <button onClick={() => { dispatch({ type: 'delete_todo', toDeleteId: item.todoId }); }}>Delete</button>
             </div>
-           
-           </li>
-          ))}
-      </ul> 
+          </li>
+        ))}
+      </ul >
     </>
   );
 }
